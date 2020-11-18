5553da71c4a295f4f2ab1143d483fa2e4d7177cf
Jure Sotošek <jure.sotosek@kcl.ac.uk>
2020-11-18 17:16:00 +0100

    Purge the DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_111010) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
