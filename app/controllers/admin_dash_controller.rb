d8ca434e4ef6267773b45a009f5b2f9c0cdd914d
Jure Sotošek <jure.sotosek@kcl.ac.uk>
2020-12-06 13:14:13 +0100

    finish up admin_dash

--- app/controllers/admin_dash_controller.rb
+++ app/controllers/admin_dash_controller.rb
@@ -2,9 +2,9 @@ class AdminDashController < ApplicationController
     def root
         p current_user
         if current_user == nil || current_user.admin == false
-            p "hej"
             redirect_to '/'
-        else @users = User.where(:admin => false)
+        else
+            @users = User.where(:admin => false)
         end
     end
 
@@ -22,6 +22,9 @@ class AdminDashController < ApplicationController
             redirect_to '/'
         else
             @account = Account.find(params[:account_id])
+            @user = @account.user
+            @accounts = @user.accounts
+            @transactions = @account.transactions
         end
     end
 
@@ -29,6 +32,7 @@ class AdminDashController < ApplicationController
         if current_user == nil || current_user.admin == false
             redirect_to '/'
         else
+          @users = User.where(:admin => false)
           @user = User.new
         end
     end
@@ -37,13 +41,20 @@ class AdminDashController < ApplicationController
       if current_user == nil || current_user.admin == false
           redirect_to '/'
       else
-        @user = User.new(user_params)
-      if @user.save
-        redirect_to('/admin_dash')
-      else
-        render('create_user')
-   end
-    end
+        p params
+        @user = User.create({
+          :email => params[:user][:email],
+          :name => params[:user][:name],
+          :password => params[:user][:password],
+          :password_confirmation => params[:user][:password],
+          :admin => false
+        })
+        if @user.save
+          redirect_to('/admin_dash')
+        else
+          redirect_to('/admin_dash/create/user')
+        end
+      end
     end
 
     def create_account
@@ -51,6 +62,7 @@ class AdminDashController < ApplicationController
             redirect_to '/'
         else
           @user = User.find(params[:user_id])
+          @accounts = @user.accounts
           @account = Account.new
         end
     end
@@ -59,27 +71,52 @@ class AdminDashController < ApplicationController
       if current_user == nil || current_user.admin == false
           redirect_to '/'
       else
-          @account = Account.new(account_params)
-          @user = User.find(params[:user_id])
+          @account = Account.new({
+            :user_id => params[:user_id],
+            :name => params[:account][:name],
+            :balance => 0,
+            :currency => params[:account][:currency],
+          })
           if @account.save
-            redirect_to("/admin_dash/user/#{@user.id}")
+            redirect_to("/admin_dash/user/#{params[:user_id]}")
           else
-            render('create_account')
+            redirect_to("/admin_dash/create/account/#{params[:user_id]}")
           end
       end
     end
 
     def create_transaction
+      if current_user == nil || current_user.admin == false
+          redirect_to '/'
+      else
+        @account = Account.find(params[:account_id])
+        @user = @account.user
+        @accounts = @user.accounts
+        @transactions = @account.transactions
+        @transaction = Transaction.new
+      end
+    end
+
+    def create_transaction_post
         if current_user == nil || current_user.admin == false
             redirect_to '/'
+        else
+          @transaction = Transaction.new({
+            :account_id => params[:account_id],
+            :amount => params[:transaction][:amount],
+            :description => params[:transaction][:description],
+            :completed_on => params[:transaction][:completed_on],
+          })
+          if @transaction.save
+            @account = @transaction.account
+            @account.balance = @account.balance + @transaction.amount
+            @account.save
+            
+            redirect_to("/admin_dash/account/#{params[:account_id]}")
+          else
+            redirect_to("/admin_dash/create/transaction/#{params[:account_id]}")
+          end
         end
     end
 
-    def user_params
-      params.require(:user).permit(:email, :name, :password)
-    end
-
-    def account_params
-      params.require(:account).permit(:name, :currency, :user_id)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_01_162630) do
-  
+
   create_table "accounts", force: :cascade do |t|
     t.float "balance", null: false
     t.string "currency", null: false
