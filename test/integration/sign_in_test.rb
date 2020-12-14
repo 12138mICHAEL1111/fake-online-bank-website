04fb1187903d781179ef3665520bb9cfe694dd81
Jure Sotošek <jure.sotosek@kcl.ac.uk>
2020-12-14 14:25:37 +0100

    Add admin create tests.

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -4,10 +4,10 @@ class SignInTest < ActionDispatch::IntegrationTest
   test "redirects user to dash on valid login" do
     get "/sign_in"
 
-    user = User.new(email:'jure.sotosek@gmail.com', name:'jure', password:'123456', password_confirmation:'123456')
+    user = User.new(email:'signin_test@gmail.com', name:'jure', password:'123456', password_confirmation:'123456')
     assert user.save
 
-    post "/sign_in_post", params: { email: 'jure.sotosek@gmail.com', password: "123456" }
+    post "/sign_in_post", params: { email: 'signin_test@gmail.com', password: "123456" }
 
     assert_redirected_to "/dash"
   end
@@ -15,10 +15,10 @@ class SignInTest < ActionDispatch::IntegrationTest
   test "redirects admin user to admin dash on valid login" do
     get "/sign_in"
 
-    user = User.new(email:'jure.sotosek+admin@gmail.com', name:'jure', password:'123456', password_confirmation:'123456', admin: true)
+    user = User.new(email:'signin_test+admin@gmail.com', name:'jure', password:'123456', password_confirmation:'123456', admin: true)
     assert user.save
 
-    post "/sign_in_post", params: { email: 'jure.sotosek+admin@gmail.com', password: "123456" }
+    post "/sign_in_post", params: { email: 'signin_test+admin@gmail.com', password: "123456" }
 
     assert_redirected_to "/admin_dash"
   end
@@ -34,10 +34,10 @@ class SignInTest < ActionDispatch::IntegrationTest
   test "errors on unvalid password" do
     get "/sign_in"
 
-    user = User.new(email:'jure.sotosek@gmail.com', name:'jure', password:'123456', password_confirmation:'123456')
+    user = User.new(email:'signin_test@gmail.com', name:'jure', password:'123456', password_confirmation:'123456')
     assert user.save
 
-    post "/sign_in_post", params: { email: 'jure.sotosek@gmail.com', password: "wrongpassword" }
+    post "/sign_in_post", params: { email: 'signin_test@gmail.com', password: "wrongpassword" }
 
     assert_redirected_to "/sign_in"
   end
