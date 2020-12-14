e0b083baf7e244719a85fb4496f7eb610dc9b3be
Jure Sotošek <jure.sotosek@kcl.ac.uk>
2020-12-14 13:51:39 +0100

    Rename tests.

--- test/integration/admin_edit_test.rb
+++ test/integration/admin_edit_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserDashTest < ActionDispatch::IntegrationTest
+class AdminEditTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
